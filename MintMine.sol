409be6ac52a7d65f53f2825bf4f2f3d874970e95
"raceragon1" <"raceragon1@gmail.com">
2022-02-17 12:22:18 +0530

    Redid

--- MintMine.sol
+++ MintMine.sol
@@ -1,71 +1,31 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.11;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
 
-contract MintMine is ERC721, Ownable{
-
-    uint256 tokenCounter;
+contract MintMine is ERC721URIStorage{
     
+    constructor() ERC721("MintMine","MMT"){ 
+    }
+    using Counters for Counters.Counter;
+    Counters.Counter private tokenIdCounter;
 
-    mapping(uint256 => address) NFTidToAddress;
-    mapping(uint256 => string) NFTidToTokenUri;
-    mapping(string => bool) URIs;
+    function mint(string memory _tokenURI) public{   
+        uint tokenId = tokenIdCounter.current();
 
-    event Created(uint256 value);
-    event Minter(address value);
-    event Burnt(uint256 value);
-    event Transfered(uint256 value);
-    event TransferedTo(address value);
+        _safeMint(msg.sender, tokenId,"");
+        _setTokenURI(tokenId, _tokenURI);
 
-    modifier tokenOwner(uint _nftId){
-        require(NFTidToAddress[_nftId] == msg.sender, "Not your Token");
-        _;
+        tokenIdCounter.increment();
     }
 
-
-
-    constructor() ERC721("",""){ 
-        tokenCounter = 0 ;
+    function burn(uint256 tokenId) public { 
+        _burn(tokenId);   
     }
 
-
-
-    function Mint(string memory _tokenURI) public{
-        require(!URIs[_tokenURI], "Token URI already exists");
-        URIs[_tokenURI] = true;
-        
-        uint _nftId;
-        
-        _nftId == tokenCounter;
-        NFTidToTokenUri[_nftId] = _tokenURI;
-        NFTidToAddress[_nftId] = msg.sender;
-
-       //   tokenURI(_nftId, _tokenURI);
-    
-        _safeMint(msg.sender, _nftId,"");
-        tokenCounter +=1;  
-
-        emit Minter(msg.sender); 
-        emit Created(_nftId);
-
+    function safeTransfer(address _from, address _to, uint256 tokenId) public {
+        safeTransferFrom(_from,_to,tokenId);
     }
 
-
-    function Burn(uint256 _nftId) public tokenOwner(_nftId) {
-       
-        _burn(_nftId);
-        NFTidToAddress[_nftId] = address(0);
-        emit Burnt(_nftId);
-    }
-
-    function SafeTransfer(address _from, address _to, uint256 _nftId) public tokenOwner(_nftId) {
-        safeTransferFrom(_from,_to,_nftId);
-        NFTidToAddress[_nftId] = _to;
-        
-        emit Transfered(_nftId);
-        emit TransferedTo(_to);
-    }
 }
-
